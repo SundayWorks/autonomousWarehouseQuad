aa2241138fa09f533fa4914a2da7305667899e10
AswathGI <34580074+AswathGI@users.noreply.github.com>
2017-12-25 14:11:23 +0530

    Delete RFIDL.py

--- RFIDL.py
+++
@@ -1,4 +0,0 @@
-from firebase import firebase
-firebase = firebase.FirebaseApplication('http://drone-189912.firebaseio.com/RFID',None)
-result= firebase.post('/RFID',{'user_id':'wilma','text':'hello'})
-
